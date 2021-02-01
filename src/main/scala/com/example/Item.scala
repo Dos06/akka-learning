31e07882557329b5c840ff2dfb468b61feb73f01
Dos06 <dostermorgan815@gmail.com>
2021-02-01 20:47:43 +0600

    Got rid of unnecessary files

--- src/main/scala/com/example/Item.scala
+++
@@ -1,9 +0,0 @@
-//package com.example
-//
-//import spray.json.DefaultJsonProtocol
-//
-//case class Item(name: String, price: Int)
-//
-//object ItemProtocol extends DefaultJsonProtocol {
-//  implicit val format = jsonFormat2(Item.apply)
-//}

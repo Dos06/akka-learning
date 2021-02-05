b088b63025de9c820a0cb55e23be10289aeaf638
Dos06 <dostermorgan815@gmail.com>
2021-02-06 00:22:42 +0600

    Used Slick to manage the DB

--- src/main/scala/com/example/BootSlick.scala
+++ src/main/scala/com/example/BootSlick.scala
@@ -1,19 +1,102 @@
 package com.example
 
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.server.Directives._
+import akka.stream.ActorMaterializer
 import com.example.models.{Item, ItemTable}
 import slick.jdbc.MySQLProfile.api._
-import scala.concurrent.ExecutionContext.Implicits.global
+
+import scala.concurrent.Future
 
 object BootSlick extends App {
   val db = Database.forConfig("mysql")
   lazy val items = TableQuery[ItemTable]
 
   db.run(items.schema.createIfNotExists)
-//  db.run(items += Item(0, "Ahahah", 333))
-//  db.run(items += Item(10, "))))", 999))
 
-  db.run(items.result).foreach(i => {
-    i.foreach(println)
-  })
+  implicit val system = ActorSystem("A-system")
+  implicit val materializer = ActorMaterializer
+  implicit val ec = system.dispatcher
+
+  var arr = Array[Item]()
+
+  val route =
+    path("") {
+      get {
+        complete("Home page")
+      }
+    }~
+    path("item") {
+      post {
+        parameter("name", "price".as[Int]) { (name, price) =>
+          db.run(items += Item(0, name, price))
+          complete("Item created")
+        }
+      }
+    }~
+    pathPrefix("delete") {
+      concat(
+        pathEnd {
+          complete("/item")
+        },
+        path(IntNumber) { int =>
+          delete {
+            val item = items.filter(_.id === int)
+            val action = item.delete
+            val affectedRowsCount: Future[Int] = db.run(action)
+            val sql = action.statements.head
+
+            complete(affectedRowsCount + "\n " + sql)
+          }
+        }
+      )
+    }~
+    pathPrefix("update") {
+      concat(
+        pathEnd {
+          complete("/item")
+        },
+        path(IntNumber) { int =>
+          put {
+            parameter("name", "price".as[Int]) { (name, price) =>
+              val item = items.filter(_.id === int).map(i => (i.name, i.price))
+              val action = item.update((name, price))
+              val sql = item.updateStatement
+              val affectedRowsCount: Future[Int] = db.run(action)
+
+              complete(affectedRowsCount + "\n " + sql)
+            }
+          }
+        }
+      )
+    }~
+    path("items") {
+      get {
+        complete {
+          val result = db.run(items.result).foreach(i => {
+            i.foreach(println)
+          })
+
+          result.toString
+        }
+      }
+    }~
+    pathPrefix("item") {
+      concat(
+        pathEnd {
+          complete("/item")
+        },
+        path(IntNumber) { int =>
+          val result = db.run(items.filter(_.id === int).result).foreach(x => {
+            x.foreach(println)
+          })
+
+          complete(result.toString)
+        }
+      )
+    }
+
+  val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
 
 }
--- src/main/scala/com/example/models/ItemTable.scala
+++ src/main/scala/com/example/models/ItemTable.scala
@@ -2,7 +2,7 @@ package com.example.models
 
 import slick.jdbc.MySQLProfile.api._
 
-final class ItemTable(tag: Tag) extends Table[Item](tag, "item") {
+final class ItemTable(tag: Tag) extends Table[Item](tag, "items") {
   def id = column[Int]("id", O.PrimaryKey, O.AutoInc)
   def name = column[String]("name")
   def price = column[Int]("price")
